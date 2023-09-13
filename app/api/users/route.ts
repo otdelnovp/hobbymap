06e044b9773ab55f3c27d3cc9fb085c987cd4925
Pavel Otdelnov <p4sh0k@gmail.com>
2023-09-13 20:34:14 +0300

    поправил роль по умолчанию

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -32,6 +32,7 @@ export const GET = async () => {
 
   try {
     const users = await prisma.user.findMany({
+      where: { role: { not: 'root' } },
       select: { id: true, name: true, email: true, image: true, role: true },
     });
     return NextResponse.json(users);

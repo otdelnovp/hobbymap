678683e24bf9ffffff22444c6157d34891c617f5
Pavel Otdelnov <p4sh0k@gmail.com>
2023-09-20 12:25:24 +0300

    обновил зависимости

--- @types/next-auth.d.ts
+++
@@ -1,9 +0,0 @@
-import type { User } from '@/helpers/authHelper';
-import { DefaultSession } from 'next-auth';
-
-declare module 'next-auth' {
-  interface Session {
-    user: DefaultSession['user'] & User;
-    accessToken: string;
-  }
-}
--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,16 +1,12 @@
-// @ts-nocheck
-
 import NextAuth from 'next-auth';
+import type { Adapter } from 'next-auth/adapters';
 import type { NextAuthOptions } from 'next-auth';
-import type { JWT } from 'next-auth/jwt';
 
-import GoggleProvider from 'next-auth/providers/google';
-// import FacebookProvider from 'next-auth/providers/facebook';
-
-import { Adapter } from 'next-auth/adapters';
-import { PrismaAdapter } from '@auth/prisma-adapter';
 import prisma from '@/prisma';
 import { PrismaClient } from '@prisma/client';
+import { PrismaAdapter } from '@next-auth/prisma-adapter';
+
+import GoggleProvider from 'next-auth/providers/google';
 
 export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma as PrismaClient) as Adapter,
@@ -19,29 +15,6 @@ export const authOptions: NextAuthOptions = {
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_SECRET!,
     }),
-    // FacebookProvider({
-    //   clientId: process.env.FACEBOOK_CLIENT_ID!,
-    //   clientSecret: process.env.FACEBOOK_SECRET!,
-    // }),
-    // Credentials({
-    //   credentials: {
-    //     email: { label: 'email', type: 'email', required: true },
-    //     password: { label: 'password', type: 'password', required: true },
-    //   },
-    //   async authorize(credentials) {
-    //     if (!credentials?.email || !credentials.password) return null;
-
-    //     const currentUser = users.find(user => user.email === credentials.email)
-
-    //     if (currentUser && currentUser.password === credentials.password) {
-    //       const { password, ...userWithoutPass } = currentUser;
-
-    //       return userWithoutPass as User;
-    //     }
-
-    //     return null
-    //   }
-    // })
   ],
   pages: {
     signIn: '/signin',
@@ -51,19 +24,20 @@ export const authOptions: NextAuthOptions = {
   },
   callbacks: {
     async session({ session, token }) {
-      return {
-        ...session,
-        user: token.user,
-        apiToken: token.apiToken,
-        accessToken: token.accessToken,
-      };
+      if (token)
+        return {
+          ...session,
+          user: token.user,
+          accessToken: token.accessToken,
+        };
+      return session;
     },
     async jwt({ token, user, trigger, session }) {
       if (trigger === 'update') {
         return { ...token, accessToken: session.accessToken, user: session.user };
       }
       if (typeof user !== 'undefined') {
-        return { ...token, user: user as JWT };
+        return { ...token, user: user };
       }
       return token;
     },

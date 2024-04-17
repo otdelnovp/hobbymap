e2ec0cfa1cec7968614c044f2a0369ae34455f17
Pavel Otdelnov <p4sh0k@gmail.com>
2024-04-17 10:16:04 +0300

    обновил иконки

--- src/shared/ui/page-title.tsx
+++ src/shared/ui/page-title.tsx
@@ -2,19 +2,23 @@ import { cn } from "@/shared/ui/utils";
 
 interface Props extends React.HTMLAttributes<HTMLDivElement> {
   size?: "large" | "middle" | "small";
+  description?: string;
 }
 
-function PageTitle({ size, className, ...props }: Props) {
+function PageTitle({ size, description, className, ...props }: Props) {
   return (
-    <h1
-      className={cn(
-        "text-2xl lg:text-3xl font-extrabold tracking-tight mb-6",
-        size === "middle" && "text-xl lg:text-2xl font-bold mb-4",
-        size === "small" && "text-lg lg:text-xl font-medium mb-2",
-        className,
-      )}
-      {...props}
-    />
+    <>
+      <h1
+        className={cn(
+          "scroll-m-20 text-2xl lg:text-3xl font-extrabold tracking-tight mb-6",
+          size === "middle" && "text-xl lg:text-2xl font-bold mb-4",
+          size === "small" && "text-lg lg:text-xl font-medium mb-2",
+          className,
+        )}
+        {...props}
+      />
+      {description && <p className="text-muted-foreground">{description}</p>}
+    </>
   );
 }
 
